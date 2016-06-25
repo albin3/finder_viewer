d197c6251df8201acd4b44e0034daff86ff74ee9
马肱 <binwei.zbw@alibaba-inc.com>
2016-06-25 20:30:02 +0800

    feature: view files and directories.

---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+let app = require('koa')();
+let fs = require('fs');
+let urlencode = require('urlencode');
+let thunkify = require('thunkify');
+
+let render = require('co-views')(
+  __dirname + '/views',
+  {map: { html: 'swig' }}
+);
+
+let stat = thunkify(fs.stat);
+let readFile = thunkify(fs.readFile);
+let readDir = thunkify(fs.readdir);
+
+let config = require('./config');
+let rootDir = config.rootDir;
+
+function* processDir() {
+  let url = this.url;
+  let dir = yield readDir(rootDir + url);
+  dir = dir.filter(d => {
+    return !/^\..+/.test(d);
+  });
+  let isdir = yield dir.map(d => {
+    let path = rootDir + url + d;
+    return function* () {
+      let s = yield stat(path);
+      return s.isDirectory();
+    }
+  });
+  let dirMeta = isdir.map((b, idx) => {
+    return {
+      name: dir[idx],
+      url: url + dir[idx] + (b ? '/' : ''),
+      target: b ? '' : '_blank',
+      dir: b
+    }
+  });
+
+  let preDir = /(.+\/).+\/?$/.exec(url);
+  if (preDir) {
+    preDir = preDir[1];
+  } else {
+    preDir = '/';
+  }
+  dirMeta.unshift({
+    name: '..',
+    url: preDir,
+    target: '',
+    dir: true
+  });
+  this.body = yield render('dir', {
+    title: url,
+    dir: url,
+    dirMeta: dirMeta
+  });
+}
+
+function* processFile() {
+  let url = this.url;
+  this.type = 'text/plain';
+  if (/.+[\.jpg|\.png|\.jpeg|\.gif|\.bmp]$/.test(url)) {
+    this.type = 'image/png';
+  }
+  if (/.+[\.json]$/.test(url)) {
+    this.type = 'application/json';
+  }
+  this.body = yield readFile(rootDir + url);
+}
+
+app.use(function*() {
+  let req = this.req;
+  let url = req.url || '/';
+  this.url = urlencode.decode(url, 'utf8');
+  try {
+    let s = yield stat(rootDir + this.url);
+    if (s.isDirectory()) {
+      yield processDir;
+    } else {
+      yield processFile;
+    }
+  } catch (e) {
+    console.log(e);
+    throw e;
+    return this.body = '不是正常目录,无法访问';
+  }
+});
+
+app.listen(3333);
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+## view files and directories in browser.
+
+- Suport OS  `osx`、`linux`
+
+
+## usage
+
+- ./config.js
+
+```javascript
+'use strict';
+
+module.exports = {
+  rootDir: '.' // config your rootDir to show.
+};
+```
+
+- npm start
+- [http://localhost:3333/](http://localhost:3333/)
